7782cb53a68d79591adfb729d585c478c4ecc731
Alex <133051664+CherniukAlx@users.noreply.github.com>
2023-09-07 20:08:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-  strings: ["Project Manager", "Data analytic","Web Developer"],
+  strings: ["Data analytic","Web Developer"],
 typeSpeed: 100,
 backSpeed: 100,
 loop: true
